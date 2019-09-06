30490c161b8284f3ddc67d641ae3b9d51d9b9bc8
student <student@nw46146.nwmissouri.edu>
2019-09-06 12:13:07 -0500

    Initial Commit

---
+++ Time Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Converter
+//
+//  Created by student on 9/6/19.
+//  Copyright © 2019 Bearcat Coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
